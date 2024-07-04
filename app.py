c23b598c3caad7a12a3af0426eb1cd256207a456
thispersontriestocode <jmforcoding@gmail.com>
2024-07-04 11:13:36 -0500

    Added Slider and Button to App.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+
 st.header('Tossing a Coin')
+
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experient of {number_of_trials} trials.')
 
 st.write('It is not a functional application yet. Under construction.')
