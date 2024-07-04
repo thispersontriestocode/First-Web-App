4d2a098b859a332f4eb5e92e61f6cf526fc6d696
thispersontriestocode <jmforcoding@gmail.com>
2024-07-04 13:41:32 -0500

    Added run

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import scipy
+import scipy.stats
 import streamlit as st
 import time
 
@@ -29,3 +29,4 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experient of {number_of_trials} trials.')
+    mean = toss_coin(number_of_trials)
